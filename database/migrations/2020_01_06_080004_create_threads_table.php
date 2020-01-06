079d7488856d4f704f73ba84d3bc8d9a3665d998
Karel Vendla <vendlakarel@gmail.com>
2020-01-06 10:34:44 +0200

    Thread & Reply migrations
    Create Thread & Reply migrations

---
+++ database/migrations/2020_01_06_080004_create_threads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id'); //To bind a thread to a user.
+            $table->string('title'); //To have a title for thread.
+            $table->text('body'); //To have a body for thread.
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
---
+++ database/migrations/2020_01_06_080610_create_replies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id'); //To bind replies to user.
+            $table->integer('thread_id'); //To bind replies to thread.
+            $table->text('body'); //To have a body for replies
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
